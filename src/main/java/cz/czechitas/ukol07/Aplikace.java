345c687526e323d5a8733464735e0a2b40828f8e
Veronika <v@email.com>
2025-06-18 21:10:25 +0200

    cast ukolu napsana vcetne zmen

--- src/main/java/cz/czechitas/ukol07/Aplikace.java
+++ src/main/java/cz/czechitas/ukol07/Aplikace.java
@@ -1,20 +1,31 @@
 package cz.czechitas.ukol07;
 
 import org.w3c.dom.ls.LSOutput;
+
+import java.io.IOException;
 import java.util.List;
 
 /**
  * Spouštěcí třída aplikace
  */
 public class Aplikace {
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws IOException {
+
+        KnihaSluzba knihaSluzba = new KnihaSluzba();
+
+       knihaSluzba.vypisSeznamVsechKnih();
+
+
 //TODO
         //vytvorit metodu main, v teto metode vytvorit instanci tridy KnihaSluzba
 
         //vypise celkovy pocet knih v databazi
 
-KnihaSluzba knihaSluzba = new KnihaSluzba();
-knihaSluzba.vypisSeznamVsechKnih();
+
+
+
+ // knihaSluzba.knihyOdAutora("Josef Čapek");
 
         //vypsat do konzole nazvy vsech knizek, ktere napsal Karel Capek
         //vypsat vsechny knihy vydane poprve v roce 1845. Vypis bude ve tvaru jmeno autora: nazev knihy
--- src/main/java/cz/czechitas/ukol07/KnihaSluzba.java
+++ src/main/java/cz/czechitas/ukol07/KnihaSluzba.java
@@ -2,45 +2,32 @@ package cz.czechitas.ukol07;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.w3c.dom.ls.LSOutput;
-
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.ArrayList;
 import java.util.List;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
+import java.util.stream.Stream;
 
 
 public class KnihaSluzba {
 
-    private List<Kniha> knihy;
+    private final List<Kniha> seznamKnih;
 
-    public static void main(String[] args) throws IOException {
-        List<Kniha> knihy = new ArrayList<>();
-        ObjectMapper objectMapper = new ObjectMapper();
+    public KnihaSluzba()throws IOException {
         InputStream inputStream = KnihaSluzba.class.getResourceAsStream("knihy.json");
-
-        knihy = objectMapper.readValue(inputStream,new TypeReference<ArrayList<Kniha>>(){});
-
-
-        //field typu List<Kniha>. Obsah tohoto souboru naplnte v konstruktoru tridy.
-        //pro nacteni pouzit ObjectMapper: InputStream vytvorim volani metody KnihaSluzba.class.getResourceAsStream("knihy.json").
-        //InputStream se musi zavrit po dokonceni / pouzit konstrukci try-with-resources
-        // pro samotne nacteni List pouzijte nasledujici konstrukci: objectMapper.readValue(inputStream, new TypeReference<List<Kniha>>(){}
-
+        ObjectMapper objectMapper = new ObjectMapper();
+        try (inputStream) {
+            seznamKnih = objectMapper.readValue(inputStream, new TypeReference<List<Kniha>>() {});
+        }
     }
 
-    public void vypisSeznamVsechKnih(){
-        System.out.println(knihy.toString());
+    public void vypisSeznamVsechKnih (){
+       seznamKnih.stream();
     }
+
+
 }
 
-
-
-
- /* public void vypisSeznamVsechKnih () {
+/* public void vypisSeznamVsechKnih () {
      //TODO
      // vrati seznam vsech knih
  }
